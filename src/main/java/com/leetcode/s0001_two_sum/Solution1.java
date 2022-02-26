d172a09ab95bc03228fbe3957c6ee809243e7fb6
Tomas <18819451598@163.com>
2022-02-26 18:38:04 +0800

    solution 1

--- src/main/java/com/leetcode/s0001_two_sum/Solution1.java
+++ src/main/java/com/leetcode/s0001_two_sum/Solution1.java
@@ -3,6 +3,9 @@
  */
 package com.leetcode.s0001_two_sum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * log instance is defined in Solution interface
  * this is how slf4j will work in this class:
@@ -14,6 +17,23 @@ package com.leetcode.s0001_two_sum;
  */
 class Solution1 implements Solution {
 
-    public int[] twoSum(int[] nums, int target) { return new int[0];}
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        //保存遍历过程中数字与位置的映射
+        Map<Integer, Integer> indexMap = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            //查找要找的数字是否在之前的遍历保存的位置
+            int find = target - nums[i];
+            Integer findIdx = indexMap.get(find);
+            if (findIdx != null) {
+                //要找的数字在之前的遍历已经出现
+                result[0] = findIdx;
+                result[1] = i;
+                return result;
+            }
+            indexMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
 
 }
--- src/test/java/com/leetcode/s0001_two_sum/Tester.java
+++ src/test/java/com/leetcode/s0001_two_sum/Tester.java
@@ -13,6 +13,10 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(Parameterized.class)
 public class Tester {
 
@@ -38,11 +42,17 @@ public class Tester {
     /** Initialize test cases */
     @Parameters
     public static Collection<Object[]> testcases() {
-        return Arrays.asList(new Object[][]{
-            // {},     // test case 1 (init parameters below: {para1, para2, expected})
-            // {},     // test case 2 (init parameters below: {para1, para2, expected})
-            // {}      // test case 3 (init parameters below: {para1, para2, expected})
-        });
+        int[] nums1 = new int[]{1,2,3,4,5,6};
+        int target1 = 9;
+        int[] result1 = new int[]{3,4};
+        Object[] case1 = new Object[]{nums1, target1, result1};
+
+        int[] nums2 = new int[]{1,2,3,4,9,10};
+        int target2 = 9;
+        int[] result2 = new int[]{};
+        Object[] case2 = new Object[]{nums2, target2, result2};
+
+        return Arrays.asList(case1,case2);
     }
 
     /**=========================== for each test case ============================== */
@@ -51,16 +61,16 @@ public class Tester {
      * Parameters for each test (initialized in testcases() method) 
      * You can change the type of parameters
      */
-    // private Object para1;                       // parameter 1
-    // private Object para2;                       // parameter 2
-    // private Object expected;                    // parameter 3 (expected answer)
+     private int[] para1;                       // parameter 1
+     private int para2;                       // parameter 2
+     private int[] expected;                    // parameter 3 (expected answer)
 
     /** This constructor must be provided to run parameterized test. */
-    public Tester(Object para1, Object para2, Object expected) {
+    public Tester(int[] para1, int para2, int[] expected) {
            // initialize test parameters
-    //     this.para1 = para1; 
-    //     this.para2 = para2;
-    //     this.expected = expected;
+         this.para1 = para1;
+         this.para2 = para2;
+         this.expected = expected;
     }
 
     /** Execute before each test method in this class is executed. */
@@ -70,14 +80,14 @@ public class Tester {
     /** Executed as a test case. */
     @Test
     public void test() {
-        //
-        // Object actual = solution.your-method(para1, para2);
-        //
-        // assertThat(actual, is(equalTo(expected)));
-        //
-        // if (LOGGER.isDebugEnabled()) {
-        //     LOGGER.debug("your-method() pass unit test!");
-        // }
+
+         Object actual = solution.twoSum(para1, para2);
+
+         assertThat(actual, is(equalTo(expected)));
+
+         if (LOGGER.isDebugEnabled()) {
+             LOGGER.debug("twoSum({},{})={} pass unit test!", para1,para2,expected);
+         }
     }
 
     /** Execute after each test method in this class is executed. */
